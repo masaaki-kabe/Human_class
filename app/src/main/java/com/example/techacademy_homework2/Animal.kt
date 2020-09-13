43261cc85f3b3550a50fd7aa7e486d142e19071e
Masaaki Kabe <net37296@ai.ayu.ne.jp>
2020-09-13 16:58:12 +0900

    課題02　再提出

--- app/src/main/java/com/example/techacademy_homework2/Animal.kt
+++ app/src/main/java/com/example/techacademy_homework2/Animal.kt
@@ -3,13 +3,12 @@ package com.example.techacademy_homework2
 abstract class Animal {
     var name:String
     var age:Int
-    var hobby:String
+
 
     //コンストラクタ
-    constructor(name:String,age:Int,hobby:String){
+    constructor(name:String,age:Int){
         this.name=name
         this.age=age
-        this.hobby=hobby
     }
 
     abstract fun say()
--- app/src/main/java/com/example/techacademy_homework2/Human.kt
+++ app/src/main/java/com/example/techacademy_homework2/Human.kt
@@ -4,9 +4,13 @@ import android.util.Log
 
 class Human:Animal,Thinkable{
 
+    var hobby:String
 
-    constructor(name:String,age:Int,hobby:String):super(name,age,hobby){
+    constructor(name:String,age:Int,hobby:String):super(name,age){
+        this.hobby=hobby
     }
+
+
 
 
 
